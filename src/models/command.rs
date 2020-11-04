22c82cbc9f54dd4f883f68d25f25682d68d200a7
Altagos <altagos@posteo.de>
2020-11-04 20:27:32 +0100

    added: running a command #1

--- src/models/command.rs
+++ src/models/command.rs
@@ -12,7 +12,7 @@ pub struct Command {
 }
 
 impl Command {
-    pub fn run(&self, ctx: &'static Context, msg: &'static Message) -> CommandResult<(), Box<dyn std::error::Error + '_>> {
+    pub fn run(&self, ctx: Context, msg: Message) -> CommandResult<(), Box<dyn std::error::Error + '_>> {
         let engine = Engine::new();
         let mut scope = Scope::new();
 
